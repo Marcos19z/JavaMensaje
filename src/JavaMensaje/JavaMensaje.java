080419368b7167656fbd73a444cffba47a66d642
Marcos19z <149180130+Marcos19z@users.noreply.github.com>
2023-11-10 20:23:45 +0100

    Añadido de botones JavaMensaje.java

--- src/JavaMensaje/JavaMensaje.java
+++ src/JavaMensaje/JavaMensaje.java
@@ -5,6 +5,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
+import javax.swing.JToggleButton;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class JavaMensaje {
 
@@ -45,6 +49,41 @@ public class JavaMensaje {
 		JPanel panel_principal = new JPanel();
 		frmEjemploRemoto.getContentPane().add(panel_principal, BorderLayout.CENTER);
 		panel_principal.setLayout(new BorderLayout(0, 0));
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String mensaje="Boton pulsado.";
+			
+			}
+		});
+		panel_principal.add(btnNewButton, BorderLayout.WEST);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String mensaje="Boton pulsado.";
+			}
+		});
+		panel_principal.add(btnNewButton_1, BorderLayout.SOUTH);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String mensaje="Boton pulsado.";
+			}
+		});
+		panel_principal.add(btnNewButton_2, BorderLayout.EAST);
+		
+		JButton btnNewButton_3 = new JButton("New button");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			
+				String mensaje="Boton pulsado.";
+			
+			}
+		});
+		panel_principal.add(btnNewButton_3, BorderLayout.CENTER);
 	}
 
 }
